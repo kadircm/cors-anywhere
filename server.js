8d9de51e19b21c402bf09f5e8b3cd27672096338
Kadir C. <145705997+kadircm@users.noreply.github.com>
2025-01-07 03:50:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -26,8 +26,6 @@ cors_proxy.createServer({
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
-    'cookie',
-    'cookie2',
     // Strip Heroku-specific headers
     'x-request-start',
     'x-request-id',
